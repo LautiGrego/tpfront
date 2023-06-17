f3f4dba3ab9855358cc29b787ce53316fb2ffeb0
Maximo <91247@sistemas.frc.utn.edu.ar>
2023-06-17 17:09:48 -0300

    Front vehiculos

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "../App.css"
 import { NavLink } from "react-router-dom";
 
 
 function Menu() {
   return (
-<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+<nav id="menu" class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div class="container-fluid">
     <a class="navbar-brand">10</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
